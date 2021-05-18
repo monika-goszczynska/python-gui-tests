65b52d08325bb830e7b95bca418c1101eda71d35
monika-goszczynska <monika.gx@gmail.com>
2021-05-18 08:41:26 +0200

    Work with GUI in Windows 2

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from fixture.application import Application
 
 @pytest.fixture(scope="session")
 def app(request):
-    fixture = Application("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook.exe")
+    fixture = Application("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook")
     request.addfinalizer(fixture.destroy)
     return fixture
