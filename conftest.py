ed6ec1c56b45081053757598ae9e4bc3c9d12400
monika-goszczynska <monika.gx@gmail.com>
2021-05-18 07:57:28 +0200

    Work with GUI in Windows 1

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+from fixture.application import Application
+
+
+@pytest.fixture(scope="session")
+def app(request):
+    fixture = Application("C:\\Program Files (x86)\\GAS Softwares\\Free Address Book\\AddressBook")
+    request.addfinalizer(fixture.destroy)
+    return fixture
